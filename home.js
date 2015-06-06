1bc5c34e29e31b8b337784e85107c69ac6494112
hkgsherlock <hkgsherlock@gmail.com>
2015-06-07 00:11:06 +0800

    moving home css & js into single files

---
+++ home.js
@@ -0,0 +1,114 @@
+$('#top img.profile').click(function() {
+    $('#profile-set-panel').toggle();
+});
+
+/*********** ↓ filter map ↓ ***********/
+$('#filter-location-selector').offset({
+    top: $('button#filter-location').offset().top + 36,
+    left: $('button#filter-location').offset().left
+});
+
+$('button#filter-location').click(function(e) {
+    $($(e.target).data('bound')).toggle();
+    $('.filter-menu').toggleClass('expanded');
+});
+
+$('.filter-location:not(#filter-location-lv1)').prepend(
+    $('<button>')
+        .text('Back to parent...')
+        .addClass('btn btn-default')
+        .attr('type', 'checkbox')
+        .attr('autocomplete', 'off')
+        .click(function(e) {
+            $(
+                $(e.target).removeClass('active')
+                    .parent().hide()
+                    .data('parent')
+            ).show();
+        })
+);
+
+$('.filter-location label.btn[data-child]').dblclick(function(e) {
+    var t = $(e.target);
+    t.parent()
+        .hide()
+        .find('input[type=checkbox]:checked').each(function(i,e) {
+            $(e).prop('checked',false)
+                .parent().removeClass('active');
+        });
+    $(t.data('child')).show();
+});
+
+$('button[name=done]').click(function(e) {
+    var saving = {
+        level: 0,
+        items: []
+    };
+    var frm = $($(e.target).data('form'));
+    var sel = frm.find('.filter-location:visible');
+    var txtArr = [];
+    saving.level = sel.data('level');
+    sel.find('input[type=checkbox]:checked').each(function(i,e) {
+        var disp = $(e).parent().text().trim();
+        var val = $(e).val();
+        saving.items[saving.items.length] = {
+            display: disp,
+            value: val
+        };
+        txtArr[txtArr.length] = disp;
+    });
+    if (txtArr.length > 0) {
+        $(frm.data('bound'))
+            .data('selected', saving)
+            .text(txtArr.join(', '));
+    } else {
+        $(frm.data('bound'))
+            .data('selected', saving)
+            .text('(None)');
+    }
+    frm.hide();
+    $('.filter-menu').removeClass('expanded');
+});
+
+$('button[name=close]').click(function(e) {
+    $($(e.target).data('form')).hide();
+    $('.filter-menu').removeClass('expanded');
+});
+
+$('.postImages').each(function(i,e) {
+    if (e.scrollWidth > 350) {
+        console.log(e);
+        $(e).parent()
+            .prepend($('<div>').addClass('postImages-scroller-right'))
+            .prepend($('<div>').addClass('postImages-scroller-left'))
+    }
+});
+/*********** ↑ filter map ↑ ***********/
+
+    // forth
+$('.postImages-scroller-right').hover(function(e) {
+    var t = $(e.target).parent().find('.postImages');
+    t.data('scrolling', setInterval(function() {
+        t.animate({
+            scrollLeft: '+=10'
+        }, 100, false);
+    }, 100));
+}, function (e) {
+    var t = $(e.target).parent().find('.postImages');
+    clearInterval(t.data('scrolling'));
+    t.stop(true,true);
+});
+
+// back
+$('.postImages-scroller-left').hover(function(e) {
+    var t = $(e.target).parent().find('.postImages');
+    t.data('scrolling', setInterval(function() {
+        t.animate({
+            scrollLeft: '-=10'
+        }, 100, false);
+    }, 100));
+}, function (e) {
+    var t = $(e.target).parent().find('.postImages');
+    clearInterval(t.data('scrolling'));
+    t.stop(true,true);
+});
